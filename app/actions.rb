32170936aa896d92f8ea39dc0c018846299b5ced
Nick Yee <njsyee@gmail.com>
2016-01-25 18:02:56 -0700

    Searches song on youtube or uses link, if given.

--- app/actions.rb
+++ app/actions.rb
@@ -5,25 +5,34 @@ end
 
 get '/music' do
   @songs = Song.all
+  pp @songs
   erb :'music/index'
 end
 
 get '/music/new' do
+  @artist = Artist.new
+  @song = Song.new
+  @song.artist = @artist
   erb :'music/new'
 end
 
 post '/music' do
-  @artist = Artist.create(
+  @artist = Artist.new(
     name: params[:artist]
-    )
+  )
+  artist_saved = @artist.save
+  pp artist_saved, @artist
   @song = Song.new(
     title: params[:title],
     artist: @artist,
     url: params[:url]  
   )
-  if @song.save
+  song_saved = @song.save
+  pp song_saved, @song
+  
+  if artist_saved && song_saved
     redirect '/music'
   else
-    erb :'messages/new'
+    erb :'/music/new'
   end
 end
